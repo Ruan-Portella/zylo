284df4008e423a401b1b973eb55366e18d94c100
Ruan Portella <ruanmorales29@gmail.com>
2025-03-27 20:12:09 -0300

    Feat: Cria subscriptions

---
+++ src/modules/subscriptions/hooks/use-subscription.ts
@@ -0,0 +1,70 @@
+import { trpc } from "@/trpc/client";
+import { useClerk } from "@clerk/nextjs";
+import { toast } from "sonner";
+
+interface useSubscriptionProps {
+  userId: string;
+  isSubscribed: boolean;
+  fromVideoId?: string;
+};
+
+export const useSubscription = ({ userId, isSubscribed, fromVideoId }: useSubscriptionProps) => {
+  const clerk = useClerk();
+
+  const utils = trpc.useUtils();
+
+  const subscribe = trpc.subscriptions.create.useMutation({
+    onSuccess: () => {
+      toast.success("Inscrito com sucesso!");
+
+      if (fromVideoId) {
+        utils.videos.getOne.invalidate({ id: fromVideoId });
+      }
+    },
+    onError: (error) => {
+      toast.error("Erro ao se inscrever. Tente novamente mais tarde.");
+
+      if (error.data?.code === "UNAUTHORIZED") {
+        toast.error("Você precisa estar logado para se inscrever.");
+        clerk.openSignIn();
+      };
+    }
+  });
+
+  const unsubscribe = trpc.subscriptions.remove.useMutation({
+    onSuccess: () => {
+      toast.success("Desinscrito com sucesso!");
+
+      if (fromVideoId) {
+        utils.videos.getOne.invalidate({ id: fromVideoId });
+      }
+    },
+    onError: (error) => {
+      toast.error("Erro ao se desinscrever. Tente novamente mais tarde.");
+
+      if (error.data?.code === "UNAUTHORIZED") {
+        toast.error("Você precisa estar logado para se desinscrever.");
+        clerk.openSignIn();
+      };
+    }
+  });
+
+  const isPending = subscribe.isPending || unsubscribe.isPending;
+
+  const onClick = () => {
+    if (isSubscribed) {
+      unsubscribe.mutate({
+        userId,
+      })
+    } else {
+      subscribe.mutate({
+        userId,
+      })
+    }
+  }
+
+  return {
+    isPending,
+    onClick,
+  }
+}
