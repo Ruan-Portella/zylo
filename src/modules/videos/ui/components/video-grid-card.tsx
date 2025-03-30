a9fa0f50d66f6fe828d95096866c7be88cb91f5f
Ruan Portella <ruanmorales29@gmail.com>
2025-03-30 19:31:44 -0300

    Feat: Cria feature de sugestões de videos

---
+++ src/modules/videos/ui/components/video-grid-card.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import { VideoGetManyOutput } from '../../types'
+import { VideoThumbnail } from './video-thumbnail';
+import { VideoInfo } from './video-info';
+
+interface VideoGridCardProps {
+  data: VideoGetManyOutput["items"][number];
+  onRemove?: () => void;
+}
+
+export const VideoGridCard = ({ data, onRemove }: VideoGridCardProps) => {
+  return (
+    <div className='flex flex-col gap-2 w-full group'>
+      <Link href={`/videos/${data.id}`} className='relative flex-none w-full aspect-video rounded-lg overflow-hidden'>
+        <VideoThumbnail
+          imageUrl={data.thumbnailUrl!}
+          previewUrl={data.thumbnailUrl!}
+          title={data.title}
+          duration={data.duration}
+        />
+      </Link>
+      <VideoInfo
+        data={data}
+        onRemove={onRemove}
+      />
+    </div>
+  )
+};
