5c2f8cf0f1d662d647ef4c9b5100f2cb7379b3c6
Ruan Portella <ruanmorales29@gmail.com>
2025-04-09 21:18:38 -0300

    Feat: Arruma error de build

--- src/modules/home/ui/components/home-navbar/search-input.tsx
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -1,12 +1,21 @@
 'use client';
 
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 import { APP_URL } from "@/constants";
 import { SearchIcon, XIcon } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 
 export const SearchInput = () => {
+  return (
+    <Suspense fallback={<Skeleton className="h-10 w-full" />}>
+      <SearchInputSuspense />
+    </Suspense>
+  )
+}
+
+export const SearchInputSuspense = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const query = searchParams.get('query') || '';
@@ -43,11 +52,11 @@ export const SearchInput = () => {
           type="text"
           placeholder="Pesquisar"
           className="w-full pl-4 py-2 pr-12 rounded-l-full border focus:outline-none focus:border-blue-500"
-          // onChange={}
+        // onChange={}
         />
         {
           value && (
-            <Button 
+            <Button
               type="button"
               variant='ghost'
               size='icon'
@@ -65,10 +74,10 @@ export const SearchInput = () => {
           )
         }
       </div>
-      <button 
-      type="submit" 
-      disabled={!value.trim()}
-      className="px-5 py-2.5 bg-gray-100 border border-l-0 rounded-r-full hover:bg-gray-200 disabled:opacity-50 disabled:cursor-not-allowed">
+      <button
+        type="submit"
+        disabled={!value.trim()}
+        className="px-5 py-2.5 bg-gray-100 border border-l-0 rounded-r-full hover:bg-gray-200 disabled:opacity-50 disabled:cursor-not-allowed">
         <SearchIcon className="size-5" />
       </button>
     </form>
